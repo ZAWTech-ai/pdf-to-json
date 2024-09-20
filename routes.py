7e645ea9017198c9cb66020e0c667985b7bb2d8e
zainbinshakoor <yammybajwa09@gmail.com>
2024-09-20 18:32:32 +0500

    feat:water-mark

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+from functions.watermark import process_pdf_with_repeating_text_watermark  # Import your watermarking function
 
 from functions.upload_file import upload_file
 
@@ -21,3 +22,22 @@ def health_check():
 
     # Return the response as JSON
     return jsonify(response)
+
+@main_bp.route('/watermark', methods=['POST'])
+def watermark_pdf():
+    data = request.json
+    # Extract parameters from the request
+    input_pdf_key = data.get('1686320832546-G2 Week 41 (revised 2022-2023) (74WVX).pdf')
+    output_pdf_key = data.get('watermark_test1.pdf')
+    watermark_text = data.get('Confidential')
+
+    # Validate input
+    if not all([bucket_name, input_pdf_key, output_pdf_key, watermark_text]):
+        return jsonify({'error': 'Missing parameters'}), 400
+
+    try:
+        # Call the watermark processing function
+        process_pdf_with_repeating_text_watermark(bucket_name, input_pdf_key, output_pdf_key, watermark_text)
+        return jsonify({'message': 'Watermark added successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
