16620a1920b82e23b5949ce966d01c113a743ed5
Waleed Umer <yammybajwa09@gmail.com>
2025-04-16 11:42:43 +0500

    s3_delete

--- functions/s3_delete.py
+++ functions/s3_delete.py
@@ -1,5 +1,16 @@
 import boto3
+import os
 from botocore.exceptions import NoCredentialsError, ClientError
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+aws_region = os.getenv("AWS_REGION")
+aws_bucket_name = os.getenv("AWS_BUCKET_NAME")
+
 
 def delete_pdf_from_s3(pdf_key):
     """
@@ -16,18 +27,20 @@ def delete_pdf_from_s3(pdf_key):
     """
     # Initialize the S3 client with provided AWS credentials
     s3 = boto3.client(
-        's3',
-        aws_access_key_id='',
-        aws_secret_access_key=''
+          's3',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    region_name=aws_region
     )
 
     try:
         # Delete the PDF from the specified S3 bucket
-        s3.delete_object(Bucket='edhubshop', Key=pdf_key)
-        return {"message": f'{pdf_key} deleted successfully from bucket'}
+        s3.delete_object(Bucket=aws_bucket_name, Key=pdf_key)
+        return {"message": f'{pdf_key} deleted successfully from bucket {aws_bucket_name}'}
     
     except NoCredentialsError:
         return {"error": "AWS credentials are not available or invalid"}
     
     except ClientError as e:
         return {"error": f'Failed to delete PDF: {str(e)}'}
+
